512cfaf8fa26359b0f3b68caf4d8116b79332341
aldopaganijr <aldojpagani@gmail.com>
2022-08-06 14:34:44 -0400

    home screen animation

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 import "./Home.scss"
 import { Link, Outlet } from 'react-router-dom'
+import { motion } from 'framer-motion'
 
 const Home = () => {
 
@@ -8,18 +9,26 @@ const Home = () => {
     <Fragment>
       <div className='home-container'>
           <div className='links-container-home'>
-            <Link className="nav-link-home" to="/AboutMe">
-              About Me
-            </Link>
-            <Link className="nav-link-home" to="/Projects">
-              Projects
-            </Link>
-            <Link className="nav-link-home" to="/Resume">
-              My Resume
-            </Link>
-            <Link className="nav-link-home" to="/ContactMe">
-              Contact Me
-            </Link>
+            <motion.div whileHover={{scale: 1.1}}>
+              <Link className="nav-link-home" to="/AboutMe">
+                About Me
+              </Link>
+            </motion.div>
+            <motion.div whileHover={{scale: 1.1}}>
+              <Link className="nav-link-home" to="/Projects">
+                Projects
+              </Link>
+            </motion.div>
+            <motion.div whileHover={{scale: 1.1}}>
+              <Link className="nav-link-home" to="/Resume">
+                My Resume
+              </Link>
+            </motion.div>
+            <motion.div whileHover={{scale: 1.1}}>
+              <Link className="nav-link-home" to="/ContactMe">
+                Contact Me
+              </Link>
+            </motion.div>
           </div>
           <div className="lowertext">
             <p className='lower-text'> React Developer</p>
