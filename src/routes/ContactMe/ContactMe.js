2d3e93a68283a9377471a40287001cbbe4fe3345
aldopaganijr <aldojpagani@gmail.com>
2022-08-05 21:34:08 -0400

    contact form done

--- src/routes/ContactMe/ContactMe.js
+++ src/routes/ContactMe/ContactMe.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import "./ContactMe.scss"
 import Footer from "../../components/Footer/Footer"
 
 const ContactMe = () => {
 
   return (
-
-    <div>
+    <Fragment>
       <h2 id="main-tag">Contact</h2>
-      <Footer />
-    </div>
-
+      <div className='contact-container'>
+        <form action="https://formsubmit.co/899c98d11aff6fd7b45223005b2302b7" method="POST">
+          <h3>Lets get in touch</h3>
+          <input type="text" id="name" placeholder="Your Name" name="name" required />
+          <input type="email" id="email" placeholder="Your Email" name="email" required />
+          <textarea id="message" rows="4" placeholder="Enter Your Message Here!" name="comments, questions?"></textarea>
+          <input type="text" name="_honey" style={{display: "none"}} />
+          <input type="text" name="_captcha" value="false" style={{display: "none"}} />
+          <button type="submit" id="submit">Send</button>
+        </form>
+      </div>
+     <Footer />
+    </Fragment>
   )
 }
 
