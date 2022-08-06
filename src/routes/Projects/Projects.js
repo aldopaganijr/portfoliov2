1c4cda152c162a586dc709f9a09d0211e2642938
aldopaganijr <aldojpagani@gmail.com>
2022-08-05 22:41:46 -0400

    projects page completed

--- src/routes/Projects/Projects.js
+++ src/routes/Projects/Projects.js
@@ -1,13 +1,50 @@
-import React from 'react'
-import "./Projects.scss"
-import Footer from "../../components/Footer/Footer"
+import React from 'react';
+import "./Projects.scss";
+import Footer from "../../components/Footer/Footer";
+import TheGrowRoom from "../../assets/theGrowRoom.png";
+import Blockbuster from "../../assets/blockbuster.png";
+import Lofi from "../../assets/lofi.png";
+import {MdLiveTv} from "react-icons/md"
+import {FaGithub} from "react-icons/fa"
+
 
 const Projects = () => {
 
   return (
-
-    <div>
-      <h2 id="main-tag">Projects</h2>
+        <div className='projects-container'>
+          <h2 id="main-tag">Projects</h2>
+          <div className="card-container">
+          <div className="project-card">
+              <img className="project-image" src={TheGrowRoom}  alt="thegrowroom" />
+              <h3>The Grow Room</h3>
+              <p className="sub-text">This project was made using React, Sass, Framer Motion, Firebase, Stripe</p>
+              <hr/>
+              <p className="sub-text">
+                  <a id="direct-link" href="https://thegrowroom.netlify.app/" alt="thegrowroom"><MdLiveTv /></a>
+                  <a id="direct-link" href="https://github.com/aldopaganijr/flower-shop" alt="thegrowroom"><FaGithub /></a>
+              </p>
+          </div>
+          <div className="project-card">
+              <img className="project-image" src={Blockbuster} alt="blockbuster" />
+              <h3>Blockbuster Movie</h3>
+              <p className="sub-text">This project was made using React, Sass, Framer Motion, Material UI</p>
+              <hr/>
+              <p className="sub-text">
+                  <a id="direct-link" href="https://blockbusterrated.netlify.app/" alt="blockbuster"><MdLiveTv /></a>
+                  <a id="direct-link" href="https://github.com/aldopaganijr/react-movie" alt="blockbuster"><FaGithub /></a>
+              </p>
+          </div>
+          <div className="project-card">
+              <img className="project-image"  src={Lofi} alt="music-player" />
+              <h3>Music Player</h3>
+              <p className="sub-text">This project was made using React & Sass, Framer Motion</p>
+              <hr/>
+              <p className="sub-text">
+                  <a id="direct-link" href="https://aldopaganijr.github.io/music-player/" alt="music-player"><MdLiveTv /></a>
+                  <a id="direct-link" href="https://github.com/aldopaganijr/music-player" alt="music-player"><FaGithub /></a>
+              </p>
+          </div>
+      </div>
       <Footer/>
     </div>
 
--- src/routes/Resume/Resume.js
+++ src/routes/Resume/Resume.js
@@ -8,6 +8,8 @@ const Resume = () => {
 
     <div>
         <h2 id="main-tag">Resume</h2>
+        <div className='resume-container'>
+        </div>
         <Footer/>
     </div>
 
