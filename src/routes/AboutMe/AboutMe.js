b99f0016f0f390433714d782b7e74f62046044ff
aldopaganijr <aldojpagani@gmail.com>
2022-08-05 23:19:40 -0400

    about page structured

--- src/routes/AboutMe/AboutMe.js
+++ src/routes/AboutMe/AboutMe.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./AboutMe.scss"
 import Footer from "../../components/Footer/Footer"
+import Me from "../../assets/me.jpg";
 
 const AboutMe = () => {
 
@@ -8,6 +9,23 @@ const AboutMe = () => {
 
     <div>
       <h2 id="main-tag">About Me</h2>
+        <div class="about-container">
+          <div class="myPic">
+              <img class="myself" src={Me} alt="self portrait" />
+          </div>
+          <div class="bio">
+              <p>I am a React Frontend Developer with a background in photography & videography. I have a serious passion for creating responsive, feature rich websites with a great user experience. If I don't have a monitor in front of me, you will most likely find me wandering around with my camera.</p>
+          </div>
+          <div class="skills">
+              <h2 id="list-heading">Skills & languages</h2>
+              <ul class="languages">
+                  <li>React</li>
+                  <li>Redux</li>
+                  <li>Hooks</li>
+                  <li>Native</li>
+              </ul>
+          </div>
+        </div>
       <Footer />
     </div>
 
