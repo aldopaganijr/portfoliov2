f4c3d00937be066e44672c56e92d213a2f7da3d6
aldopaganijr <aldojpagani@gmail.com>
2022-08-05 22:53:24 -0400

    resume iframe working

--- src/routes/Resume/Resume.js
+++ src/routes/Resume/Resume.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./Resume.scss"
 import Footer from "../../components/Footer/Footer"
+import ResumeDoc from "../../assets/mainresume.pdf"
 
 const Resume = () => {
 
@@ -9,6 +10,7 @@ const Resume = () => {
     <div>
         <h2 id="main-tag">Resume</h2>
         <div className='resume-container'>
+          <iframe id='myResume' src={ResumeDoc} title='resume' frameBorder='0' ></iframe>
         </div>
         <Footer/>
     </div>
