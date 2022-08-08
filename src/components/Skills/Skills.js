c3147cdadf5febc60f602f151f4a08b0109dff7e
aldopaganijr <aldojpagani@gmail.com>
2022-08-07 21:42:26 -0400

    skills banner update

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import "./Skills.scss"
-import {SiTypescript} from "react-icons/si"
 import {FaReact} from "react-icons/fa"
 import {FaVuejs} from "react-icons/fa"
 import {FaNodeJs} from "react-icons/fa"
 import {SiJavascript} from "react-icons/si"
 import {SiExpress} from "react-icons/si"
 import {SiMongodb} from "react-icons/si"
+import {SiSvelte} from "react-icons/si"
 
 
 
@@ -18,7 +18,7 @@ const Skills = () => {
     <div className='skills-container'>
         <ul className='icon-list'>
             <li className='icon'><SiJavascript /></li>
-            <li className='icon'><SiTypescript /></li>
+            <li className='icon'><SiSvelte /></li>
             <li className='icon'><FaVuejs /></li>
             <span className='main'><li><FaReact /></li></span>
             <li className='icon'><SiMongodb /></li>
